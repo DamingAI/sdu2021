d350487ddbd548ef9a38379a648d9433fc6bfd14
jingmingming <jingmingming@sina.com>
2021-01-11 14:57:54 +0800

    工程结构

---
+++ day01/code/myqt.py
@@ -0,0 +1,23 @@
+# 依赖第三方库
+# import 模块名
+# import 模块名  as 新名字
+
+# from  *.py import *
+# from 模块 import 模块
+
+# ---QT DEMO----
+# 桌面应用程序-->窗口---->按钮 文本 视频
+from PyQt5.QtWidgets import QApplication,QWidget
+# python 系统模块
+import sys
+# 应用程序
+app = QApplication(sys.argv)
+# 窗口组件
+box = QWidget()
+box.resize(250,250) #大小
+box.move(300,300)  #位置
+box.setWindowTitle("box")# 标题
+box.show() #显示
+#app
+status = app.exec_()
+sys.exit(status)
